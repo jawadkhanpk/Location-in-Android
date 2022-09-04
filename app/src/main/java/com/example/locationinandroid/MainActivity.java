a7bf3ee347da2d77956cdbe411c40e43532e260a
jawadkhanpk <jawadkhan.tech@gmail.com>
2022-09-04 16:02:53 +0500

    Implementing Blank clickOnListeners on Buttons

--- app/src/main/java/com/example/locationinandroid/MainActivity.java
+++ app/src/main/java/com/example/locationinandroid/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.locationinandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnStartLocation, btnStopLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnStartLocation = findViewById(R.id.idBtnStartLocation);
+        btnStopLocation = findViewById(R.id.idBtnStopLocation);
+
+        btnStartLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnStopLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
